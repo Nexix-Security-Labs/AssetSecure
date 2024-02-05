04faac5b8873ed907eda0c8ef83e438a440d1744
W01F_PR1M3 <32429386+w01f-5h@users.noreply.github.com>
2024-02-05 23:25:07 +0530

    Update develop.sh

--- docker/develop.sh
+++ docker/develop.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #docker run -v docker start mysql
-# docker run --name assetsecure-mysql -e MYSQL_ROOT_PASSWORD=my_crazy_super_secret_root_password -e MYSQL_DATABASE=assetsecureit -e MYSQL_USER=assetsecureit -e MYSQL_PASSWORD=whateverdood -d mysql
-docker run -d assetsecure-mysql
-#docker run -d -v ~/Documents/assetsecureyhead/assetsecure-it/:/var/www/html -p $(boot2docker ip)::80   --link assetsecure-mysql:mysql --name=assetsecureit assetsecureit
-docker run --link assetsecure-mysql:mysql -d -p 40000:80 --name=assetsecure-it -v ~/Documents/assetsecureyhead/assetsecure-it/:/var/www/html \
--v ~/Documents/assetsecureyhead/assetsecure-it-storage:/var/lib/assetsecureit --env-file docker.env assetsecure-test
+# docker run --name snipe-mysql -e MYSQL_ROOT_PASSWORD=my_crazy_super_secret_root_password -e MYSQL_DATABASE=snipeit -e MYSQL_USER=snipeit -e MYSQL_PASSWORD=whateverdood -d mysql
+docker run -d snipe-mysql
+#docker run -d -v ~/Documents/snipeyhead/snipe-it/:/var/www/html -p $(boot2docker ip)::80   --link snipe-mysql:mysql --name=snipeit snipeit
+docker run --link snipe-mysql:mysql -d -p 40000:80 --name=snipe-it -v ~/Documents/snipeyhead/snipe-it/:/var/www/html \
+-v ~/Documents/snipeyhead/snipe-it-storage:/var/lib/snipeit --env-file docker.env snipe-test
